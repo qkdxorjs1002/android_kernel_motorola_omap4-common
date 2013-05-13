077ca0b757804f363942aea3b27cff7600ed8ddc
dtrail <mnl.manz@gmail.com>
2013-05-13 16:28:52 +0200

    Removed 3rd active GPU scaling step and revert to GPU OC val = 0 default

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -78,7 +78,7 @@ static bool omap_cpufreq_ready;
 static bool omap_cpufreq_suspended;
 static unsigned int screen_off_max_freq;
 
-int oc_val = 1;
+int oc_val = 0;
 
 static unsigned int omap_getspeed(unsigned int cpu)
 {
