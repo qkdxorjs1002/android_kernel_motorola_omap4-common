ac14220cba9b4501be4b07dc86755dec8b5ad8d0
dtrail <mnl.manz@gmail.com>
2013-05-16 10:59:12 +0200

    added Lulzactivew governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -382,6 +382,9 @@ extern struct cpufreq_governor cpufreq_gov_interactivex;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_hyper;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_hyper) 
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEW)
+extern struct cpufreq_governor cpufreq_gov_lulzactivew;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lulzactivew) 
 #endif
 
 
