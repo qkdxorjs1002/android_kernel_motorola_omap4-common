3b88c9fd166e691ca7dda24395e1d29dd52aac20
dtrail <mnl.manz@gmail.com>
2013-05-25 17:50:56 +0200

    Disable SR vmax sysfs interface - only to export a STABLE-BRANCH

--- arch/arm/mach-omap2/smartreflex.c
+++ arch/arm/mach-omap2/smartreflex.c
@@ -80,8 +80,8 @@ static u32 coremax = OMAP4_VP_CORE_VLIMITTO_VDDMAX; */
 #define LOWFLOOR 700000
 #define LOWCEILING 1000000
 
-#define HIGHFLOOR 1150000
-#define HIGHCEILING 1410000
+/*#define HIGHFLOOR 1150000
+#define HIGHCEILING 1450000*/
 
 
 static inline void sr_write_reg(struct omap_sr *sr, unsigned offset, u32 value)
@@ -1088,7 +1088,7 @@ static int omap_sr_vmin_show(void *data, u64 *val)
   return 0;
 }
 
-static int omap_sr_vmax_show(void *data, u64 *val)
+/*static int omap_sr_vmax_show(void *data, u64 *val)
 {
   struct omap_sr *sr = (struct omap_sr *) data;
 
@@ -1101,7 +1101,7 @@ static int omap_sr_vmax_show(void *data, u64 *val)
         else if (!(strcmp(sr->voltdm->name, "iva"))) *val = ivamax;
         else if (!(strcmp(sr->voltdm->name, "core"))) *val = coremax;
   return 0;
-}
+}*/
 
 static int omap_sr_vmin_store(void *data, u64 val)
 {
@@ -1168,7 +1168,7 @@ static int omap_sr_vmin_store(void *data, u64 val)
   return 0;
 }
 
-static int omap_sr_vmax_store(void *data, u64 val)
+/*static int omap_sr_vmax_store(void *data, u64 val)
 {
         struct omap_sr *sr_info = (struct omap_sr *) data;
         struct omap_vp_instance *vp;
@@ -1198,7 +1198,7 @@ static int omap_sr_vmax_store(void *data, u64 val)
     mpumax = OMAP4_VP_MPU_VLIMITTO_VDDMAX;
     } else if (srtype==2) {
     vddmax = sr_info->voltdm->pmic->uv_to_vsel(OMAP4_VP_IVA_VLIMITTO_VDDMAX);
-    ivamax = OMAP4_VP_IVA_VLIMITTO_VDDMAX;
+    ivamin = OMAP4_VP_IVA_VLIMITTO_VDDMIN;
     } else {
     vddmax = sr_info->voltdm->pmic->uv_to_vsel(OMAP4_VP_CORE_VLIMITTO_VDDMAX);
     coremax = OMAP4_VP_CORE_VLIMITTO_VDDMAX;
@@ -1231,7 +1231,7 @@ static int omap_sr_vmax_store(void *data, u64 val)
         sr_stop_vddautocomp(sr_info);
         sr_start_vddautocomp(sr_info);
   return 0;
-}
+} */
 
 /* PM Debug Fs enteries to enable disable smartreflex. */
 static int omap_sr_autocomp_show(void *data, u64 *val)
@@ -1280,8 +1280,8 @@ DEFINE_SIMPLE_ATTRIBUTE(pm_sr_fops, omap_sr_autocomp_show,
 DEFINE_SIMPLE_ATTRIBUTE(pm_sr_fops2, omap_sr_vmin_show,
     		omap_sr_vmin_store, "%llu\n");
 
-DEFINE_SIMPLE_ATTRIBUTE(pm_sr_fops3, omap_sr_vmax_show,
-    		omap_sr_vmax_store, "%llu\n");
+/*DEFINE_SIMPLE_ATTRIBUTE(pm_sr_fops3, omap_sr_vmax_show,
+    		omap_sr_vmax_store, "%llu\n"); */
 
 static int __init omap_sr_probe(struct platform_device *pdev)
 {
@@ -1394,8 +1394,8 @@ static int __init omap_sr_probe(struct platform_device *pdev)
 			sr_info->dbg_dir, (void *)sr_info, &pm_sr_fops);
 	(void) debugfs_create_file("vmin", S_IRUGO | S_IWUSR,
 			sr_info->dbg_dir, (void *)sr_info, &pm_sr_fops2);
-	(void) debugfs_create_file("vmax", S_IRUGO | S_IWUSR,
-			sr_info->dbg_dir, (void *)sr_info, &pm_sr_fops3);
+	/*(void) debugfs_create_file("vmax", S_IRUGO | S_IWUSR,
+			sr_info->dbg_dir, (void *)sr_info, &pm_sr_fops3);*/
 	(void) debugfs_create_x32("errweight", S_IRUGO, sr_info->dbg_dir,
 			&sr_info->err_weight);
 	(void) debugfs_create_x32("errmaxlimit", S_IRUGO, sr_info->dbg_dir,
