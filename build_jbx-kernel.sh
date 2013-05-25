546f94a893ebb6291b85f829e49772ff0d907959
dtrail <mnl.manz@gmail.com>
2013-05-25 21:41:46 +0200

    Added build script

---
+++ build_jbx-kernel.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Build script for JBX-Kernel
+
+
+
+# Exporting the toolchain (You may change this to your local toolchain location)
+export PATH=~/build/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin:$PATH
+
+# export the flags (leave this alone)
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-eabi-
+export LOCALVERSION="-JBX-0.5-Hybrid"
+
+# execute build command with "-j4 core flag" 
+# (You may change this to the count of your CPU.
+# Don't set it too high or it will result in a non-bootable kernel.
+
+make -j4
