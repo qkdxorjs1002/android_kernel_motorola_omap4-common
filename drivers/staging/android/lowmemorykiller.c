bb54733fd5640d9df668e44b413eaf37dbb5c564
dtrail <mnl.manz@gmail.com>
2013-06-21 13:25:28 +0200

    Some fixes...

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -45,10 +45,10 @@ static int lowmem_adj[6] = {
 };
 static int lowmem_adj_size = 4;
 static size_t lowmem_minfree[6] = {
-	7 * 512,	/* JBX-Kernel */
-	6 * 1024,	/* JBX-Kernel */
-	8 * 1024,	/* JBX-Kernel */
-	20 * 1024,	/* JBX-Kernel */
+	3 * 512,	/* 6MB */
+	2 * 1024,	/* 8MB */
+	4 * 1024,	/* 16MB */
+	16 * 1024,	/* 64MB */
 };
 static int lowmem_minfree_size = 4;
 
