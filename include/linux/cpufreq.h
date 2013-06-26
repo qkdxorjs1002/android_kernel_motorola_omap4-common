ed19d46077377b49f11b2cbca51c93d7213ee0ef
dtrail <mnl.manz@gmail.com>
2013-06-26 14:24:47 +0200

    Add CPUFREQ Abyssplug

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -391,6 +391,9 @@ extern struct cpufreq_governor cpufreq_gov_hyper;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEW)
 extern struct cpufreq_governor cpufreq_gov_lulzactivew;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lulzactivew) 
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG)
+extern struct cpufreq_governor cpufreq_gov_abyssplug;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_abyssplug) 
 #endif
 
 
