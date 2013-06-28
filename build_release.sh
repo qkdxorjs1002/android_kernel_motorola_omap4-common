42801187b40eef1e465920c641d1e1af34e720cb
dtrail <mnl.manz@gmail.com>
2013-06-28 10:49:32 +0200

    Build script: New package location

--- build_release.sh
+++ build_release.sh
@@ -2,12 +2,14 @@
 set -m
 
 # Build script for JBX-Kernel RELEASE
+echo "Cleaning out kernel source directory..."
 make mrproper
 make ARCH=arm distclean
 
 # We build the kernel and its modules first
 # Launch execute script in background
 # First get tags in shell
+echo "Cleaning out Android source directory..."
 cd ~/android/system
 make ARCH=arm distclean
 make mrproper
@@ -15,15 +17,19 @@ source build/envsetup.sh
 lunch 9
 
 # built kernel & modules
+echo "Building modules..."
 make -j8 TARGET_KERNEL_SOURCE=/home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common/ TARGET_KERNEL_CONFIG=mapphone_OCE_defconfig CONFIG_APANIC_PLABEL="mmcblk1p20" $OUT/boot.img
 
 # We don't use the kernel but the modules
-cp out/target/product/umts_spyder/system/lib/modules/* ~/razr_kdev_kernel/android_kernel_motorola_omap4-common/built/rls/system/lib/modules/
+echo "Copying modules to package folder"
+cp -r /home/mnl-manz/android/system/out/target/product/spyder/system/lib/modules/* /home/mnl-manz/razr_kdev_kernel/built/rls/system/lib/modules/
 
 # Switch to kernel folder
+echo "Entering kernel source..."
 cd ~/razr_kdev_kernel/android_kernel_motorola_omap4-common
 
 # Exporting the toolchain (You may change this to your local toolchain location)
+echo "Starting configuration of kernel..."
 export PATH=~/build/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin:$PATH
 
 # export the flags (leave this alone)
@@ -38,16 +44,17 @@ export LOCALVERSION="-JBX-0.7a-Hybrid"
 # execute build command with "-j4 core flag" 
 # (You may change this to the count of your CPU.
 # Don't set it too high or it will result in a non-bootable kernel.
+echo "Building..."
 make -j4
 
 # Copy and rename the zImage into nightly/nightly package folder
 # Keep in mind that we assume that the modules were already built and are in place
 # So we just copy and rename, then pack to zip including the date
-cp arch/arm/boot/zImage built/rls/system/etc/kexec/kernel
-touch built/rls/system/etc/sysctl.conf
-cp prebuilt/01kernel built/rls/system/etc/init.d
-cd built/rls
+echo "Packaging flashable Zip file..."
+cp -f /arch/arm/boot/zImage /home/mnl-manz/razr_kdev_kernel/built/rls/system/etc/kexec/kernel
+
+cd /home/mnl-manz/razr_kdev_kernel/built/rls
 zip -r "JBX-Kernel-Hybrid_$(date +"%Y-%m-%d").zip" *
-cp "JBX-Kernel-Hybrid_$(date +"%Y-%m-%d").zip" ~/razr_kdev_kernel/android_kernel_motorola_omap4-common/built
+cp "JBX-Kernel-Hybrid_$(date +"%Y-%m-%d").zip" /home/mnl-manz/razr_kdev_kernel/built
 
 echo "done"
