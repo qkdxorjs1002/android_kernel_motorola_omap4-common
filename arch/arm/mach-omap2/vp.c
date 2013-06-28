d157f982cb22bdff026d5a1e6295e71c2e237de1
dtrail <mnl.manz@gmail.com>
2013-06-28 16:47:29 +0200

    Revert "OMAP3+: PM: VP: check only the VPINIDLE status bit"
    This reverts commit dae19fc5862a70980b7a91939e407f7f90a842ce.

--- arch/arm/mach-omap2/vp.c
+++ arch/arm/mach-omap2/vp.c
@@ -195,8 +195,8 @@ int omap_vp_forceupdate_scale(struct voltagedomain *voltdm,
 	 * This is an additional allowance to ensure we are in proper state
 	 * to enter into forceupdate state transition.
 	 */
-	omap_test_timeout((voltdm->read(vp->vstatus) & vp->common->vstatus_vpidle),
-			VP_IDLE_TIMEOUT, timeout);
+	omap_test_timeout((voltdm->read(vp->vstatus)), VP_IDLE_TIMEOUT,
+			timeout);
 
 	if (timeout >= VP_IDLE_TIMEOUT)
 		_vp_controlled_err(vp, voltdm,
@@ -403,8 +403,8 @@ void omap_vp_disable(struct voltagedomain *voltdm)
 	 * Wait for VP idle Typical latency is <2us. Maximum latency is ~100us
 	 * Depending on if we catch VP in the middle of an SR operation.
 	 */
-	omap_test_timeout((voltdm->read(vp->vstatus) & vp->common->vstatus_vpidle),
-			VP_IDLE_TIMEOUT, timeout);
+	omap_test_timeout((voltdm->read(vp->vstatus)),
+			  VP_IDLE_TIMEOUT, timeout);
 
 	if (timeout >= VP_IDLE_TIMEOUT)
 		pr_warning("%s: vdd_%s idle timedout before disable\n",
@@ -418,8 +418,8 @@ void omap_vp_disable(struct voltagedomain *voltdm)
 	/*
 	 * Wait for VP idle Typical latency is <2us. Maximum latency is ~100us
 	 */
-	omap_test_timeout((voltdm->read(vp->vstatus) & vp->common->vstatus_vpidle),
-			VP_IDLE_TIMEOUT, timeout);
+	omap_test_timeout((voltdm->read(vp->vstatus)),
+			  VP_IDLE_TIMEOUT, timeout);
 
 	if (timeout >= VP_IDLE_TIMEOUT)
 		pr_warning("%s: vdd_%s idle timedout after disable\n",
