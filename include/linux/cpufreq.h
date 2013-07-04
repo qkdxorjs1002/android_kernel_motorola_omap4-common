b046f964e4cbc50ad0acda1d8dd1d878cee92bbe
dtrail <mnl.manz@gmail.com>
2013-07-05 00:43:42 +0200

    Add Intellidemand, add PHASE 2 option to ondemand governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -24,6 +24,8 @@
 
 #define CPUFREQ_NAME_LEN 16
 
+//int lock_policy_rwsem_write(int cpu);
+//void unlock_policy_rwsem_write(int cpu);
 
 /*********************************************************************
  *                     CPUFREQ NOTIFIER INTERFACE                    *
@@ -203,9 +205,6 @@ extern int __cpufreq_driver_getavg(struct cpufreq_policy *policy,
 
 int cpufreq_register_governor(struct cpufreq_governor *governor);
 void cpufreq_unregister_governor(struct cpufreq_governor *governor);
-
-int lock_policy_rwsem_write(int cpu);
-void unlock_policy_rwsem_write(int cpu);
 
 /*********************************************************************
  *                      CPUFREQ DRIVER INTERFACE                     *
@@ -417,6 +416,9 @@ extern struct cpufreq_governor cpufreq_gov_gallimaufry;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
 extern struct cpufreq_governor cpufreq_gov_badass;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_badass) 
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellidemand)
 #endif
 
 /*********************************************************************
