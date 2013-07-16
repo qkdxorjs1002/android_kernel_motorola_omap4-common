1a881248f937f6da97c9a52a1fd781ba5f891ad4
dtrail <mnl.manz@gmail.com>
2013-07-16 20:37:09 +0200

    Fix a typo in build script

--- build_nightly.sh
+++ build_nightly.sh
@@ -63,6 +63,6 @@ cd /home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common
 git log --oneline --after="yesterday" > /home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common/changelog/Changelog_Nightly_$(date +"%Y-%m-%d")
 git add changelog/ .
 git commit -m "Added todays changelog (Nightly)"
-git push origin JBX_STABLE
+git push origin JBX
 
 echo "done"
