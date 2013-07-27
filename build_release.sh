6b83d8a7ee13117eb2b4e7498c21d63ea1c87588
dtrail <mnl.manz@gmail.com>
2013-07-27 22:12:57 +0200

    Build script updates

--- build_release.sh
+++ build_release.sh
@@ -57,5 +57,13 @@ cd /home/mnl-manz/razr_kdev_kernel/built/rls
 zip -r "JBX-Kernel-0.7d-Hybrid_$(date +"%Y-%m-%d").zip" *
 mv "JBX-Kernel-0.7d-Hybrid_$(date +"%Y-%m-%d").zip" /home/mnl-manz/razr_kdev_kernel/built
 
+# Exporting changelog to file
+echo "Exporting changelog to file: '/built/Changelog-[date]'"
+cd /home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common
+git log --oneline --after="yesterday" > /home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common/changelog/Changelog_$(date +"%Y-%m-%d")
+git log --oneline  > /home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common/changelog/Full_History_Changelog
+git add changelog/ .
+git commit -m "Added todays changelog and updated full history"
+git push origin JBX_STABLE
 
 echo "done"
