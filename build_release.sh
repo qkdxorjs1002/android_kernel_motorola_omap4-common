63c4ae8868534347347d6216e13e83ad01de7171
dtrail <mnl.manz@gmail.com>
2013-08-02 01:00:15 +0200

    Build script updates

--- build_release.sh
+++ build_release.sh
@@ -3,6 +3,7 @@ set -m
 
 # Build script for JBX-Kernel RELEASE
 echo "Cleaning out kernel source directory..."
+echo " "
 make mrproper
 make ARCH=arm distclean
 
@@ -10,7 +11,9 @@ make ARCH=arm distclean
 # Launch execute script in background
 # First get tags in shell
 echo "Cleaning out Android source directory..."
+echo " "
 cd ~/android/system
+export USE_CCACHE=1
 make ARCH=arm distclean
 make mrproper
 source build/envsetup.sh
@@ -18,18 +21,22 @@ lunch 12
 
 # built kernel & modules
 echo "Building modules..."
+echo " "
 make -j8 TARGET_KERNEL_SOURCE=/home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common/ TARGET_KERNEL_CONFIG=mapphone_OCE_defconfig CONFIG_APANIC_PLABEL="mmcblk1p20" $OUT/boot.img
 
 # We don't use the kernel but the modules
 echo "Copying modules to package folder"
+echo " "
 cp -r /home/mnl-manz/android/system/out/target/product/spyder/system/lib/modules/* /home/mnl-manz/razr_kdev_kernel/built/rls/system/lib/modules/
 
 # Switch to kernel folder
 echo "Entering kernel source..."
+echo " "
 cd ~/razr_kdev_kernel/android_kernel_motorola_omap4-common
 
 # Exporting the toolchain (You may change this to your local toolchain location)
 echo "Starting configuration of kernel..."
+echo " "
 export PATH=~/build/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin:$PATH
 
 # export the flags (leave this alone)
@@ -45,12 +52,14 @@ export LOCALVERSION="-JBX-0.7d-Hybrid"
 # (You may change this to the count of your CPU.
 # Don't set it too high or it will result in a non-bootable kernel.
 echo "Building..."
+echo " "
 make -j4
 
 # Copy and rename the zImage into nightly/nightly package folder
 # Keep in mind that we assume that the modules were already built and are in place
 # So we just copy and rename, then pack to zip including the date
 echo "Packaging flashable Zip file..."
+echo " "
 cp arch/arm/boot/zImage /home/mnl-manz/razr_kdev_kernel/built/rls/system/etc/kexec/kernel
 
 cd /home/mnl-manz/razr_kdev_kernel/built/rls
@@ -59,6 +68,7 @@ mv "JBX-Kernel-0.7d-Hybrid_$(date +"%Y-%m-%d").zip" /home/mnl-manz/razr_kdev_ker
 
 # Exporting changelog to file
 echo "Exporting changelog to file: '/built/Changelog-[date]'"
+echo " "
 cd /home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common
 git log --oneline --since="4 day ago" > /home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common/changelog/Changelog_$(date +"%Y-%m-%d")
 git log --oneline  > /home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common/changelog/Full_History_Changelog
