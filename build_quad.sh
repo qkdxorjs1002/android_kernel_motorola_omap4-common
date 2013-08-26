df9468f19838e4be7f21232427a5aac2f63cf08f
dtrail <mnl.manz@gmail.com>
2013-08-26 17:00:48 +0200

    Fix build script

--- build_quad.sh
+++ build_quad.sh
@@ -13,7 +13,7 @@ echo "Cleaning out Android source directory..."
 echo " "
 cd /home/dtrail/android/4.3
 export USE_CCACHE=1
-make mrproper
+make mrproper && make ARCH=arm distclean
 source build/envsetup.sh
 lunch cm_spyder-userdebug
 
