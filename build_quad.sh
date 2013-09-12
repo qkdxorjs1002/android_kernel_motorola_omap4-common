a741010f3f80d96f4c8e59ba2e59b2525f627f19
dtrail <mnl.manz@gmail.com>
2013-09-12 12:03:58 +0200

    build script updates

--- build_quad.sh
+++ build_quad.sh
@@ -22,7 +22,7 @@ lunch cm_spyder-userdebug
 # built kernel & modules
 echo "Building kernel and modules..."
 echo " "
-export LOCALVERSION="-JBX-1.1-Hybrid-4.3"
+export LOCALVERSION="-JBX-1.2-Hybrid-4.3"
 make -j4 TARGET_KERNEL_SOURCE=/home/dtrail/android/android_kernel_motorola_omap4-common/ TARGET_KERNEL_CONFIG=mapphone_OCE_defconfig $OUT/boot.img
 
 # We don't use the kernel but the modules
@@ -43,8 +43,8 @@ echo "Packaging flashable Zip file..."
 echo " "
 
 cd /home/dtrail/android/built/rls
-zip -r "JBX-Kernel-1.0-Hybrid-4.3_$(date +"%Y-%m-%d").zip" *
-mv "JBX-Kernel-1.0-Hybrid-4.3_$(date +"%Y-%m-%d").zip" /home/dtrail/android/out
+zip -r "JBX-Kernel-1.2-Hybrid-4.3_$(date +"%Y-%m-%d").zip" *
+mv "JBX-Kernel-1.2-Hybrid-4.3_$(date +"%Y-%m-%d").zip" /home/dtrail/android/out
 
 # Exporting changelog to file
 cd /home/dtrail/android/android_kernel_motorola_omap4-common
