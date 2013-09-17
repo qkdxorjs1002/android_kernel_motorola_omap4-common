b608baf381c2364eccd6e3dfaece7616cf285d03
dtrail <mnl.manz@gmail.com>
2013-09-17 11:01:35 +0200

    Suspend Gov: Fix early suspend

--- drivers/misc/suspend_gov.c
+++ drivers/misc/suspend_gov.c
@@ -47,11 +47,11 @@ static struct cpufreq_driver *cpufreq_driver;
 static DEFINE_SPINLOCK(cpufreq_driver_lock);
 static BLOCKING_NOTIFIER_HEAD(cpufreq_policy_notifier_list);
 
-bool suspend_gov_early_suspend_active __read_mostly = false;
+bool suspend_gov_early_suspend_active __read_mostly = true;
 static int suspend_gov;
 
-static char def_governor[16];
-static char good_governor[16] = "hotplug";
+char def_governor[16];
+char good_governor[16] = "conservative";
 
 bool change_g;
 
@@ -78,55 +78,6 @@ pure_initcall(init_cpufreq_transition_notifier_list);
 
 // cpufreq.c
 
-static int __cpufreq_governor(struct cpufreq_policy *policy,
-					unsigned int event)
-{
-	int ret;
-
-	/* Only must be defined when default governor is known to have latency
-	   restrictions, like e.g. conservative or ondemand.
-	   That this is the case is already ensured in Kconfig
-	*/
-#ifdef CONFIG_CPU_FREQ_GOV_PERFORMANCE
-	struct cpufreq_governor *gov = &cpufreq_gov_performance;
-#else
-	struct cpufreq_governor *gov = NULL;
-#endif
-
-	if (policy->governor->max_transition_latency &&
-	    policy->cpuinfo.transition_latency >
-	    policy->governor->max_transition_latency) {
-		if (!gov)
-			return -EINVAL;
-		else {
-			printk(KERN_WARNING "%s governor failed, too long"
-			       " transition latency of HW, fallback"
-			       " to %s governor\n",
-			       policy->governor->name,
-			       gov->name);
-			policy->governor = gov;
-		}
-	}
-
-	if (!try_module_get(policy->governor->owner))
-		return -EINVAL;
-
-	pr_debug("__cpufreq_governor for CPU %u, event %u\n",
-						policy->cpu, event);
-	ret = policy->governor->governor(policy, event);
-
-	/* we keep one module reference alive for
-			each CPU governed by this CPU */
-	if ((event != CPUFREQ_GOV_START) || ret)
-		module_put(policy->governor->owner);
-	if ((event == CPUFREQ_GOV_STOP) && !ret)
-		module_put(policy->governor->owner);
-
-	return ret;
-}
-
-
-
 static struct cpufreq_governor *__find_governor_s(const char *str_governor)
 {
 	struct cpufreq_governor *t;
@@ -137,54 +88,6 @@ static struct cpufreq_governor *__find_governor_s(const char *str_governor)
 
 	return NULL;
 }
-
-
-static int cpufreq_parse_governor(char *str_governor, unsigned int *policy,
-				struct cpufreq_governor **governor)
-{
-	int err = -EINVAL;
-
-	if (!cpufreq_driver)
-		goto out;
-
-	if (cpufreq_driver->setpolicy) {
-		if (!strnicmp(str_governor, "performance", CPUFREQ_NAME_LEN)) {
-			*policy = CPUFREQ_POLICY_PERFORMANCE;
-			err = 0;
-		} else if (!strnicmp(str_governor, "powersave",
-						CPUFREQ_NAME_LEN)) {
-			*policy = CPUFREQ_POLICY_POWERSAVE;
-			err = 0;
-		}
-	} else if (cpufreq_driver->target) {
-		struct cpufreq_governor *t;
-
-		mutex_lock(&cpufreq_governor_mutex);
-
-		t = __find_governor_s(str_governor);
-
-		if (t == NULL) {
-			int ret;
-
-			mutex_unlock(&cpufreq_governor_mutex);
-			ret = request_module("cpufreq_%s", str_governor);
-			mutex_lock(&cpufreq_governor_mutex);
-
-			if (ret == 0)
-				t = __find_governor_s(str_governor);
-		}
-
-		if (t != NULL) {
-			*governor = t;
-			err = 0;
-		}
-
-		mutex_unlock(&cpufreq_governor_mutex);
-	}
-out:
-	return err;
-}
-
 
 static int __cpufreq_set_policy_s(struct cpufreq_policy *data,
 				struct cpufreq_policy *policy)
@@ -274,14 +177,14 @@ error_out:
 
 
 
-// static char *sgov;
-int gov_val = 2;
-/*static char *cpufreq_sysfs_place_holder="/sys/devices/system/cpu/cpu%i/cpufreq/scaling_governor";
-static char cpufreq_gov_default[32];
-static char *cpufreq_gov_ondemand = "ondemand";
-static char *cpufreq_gov_interactive = "interactive";
-static char *cpufreq_gov_ondemandx = "ondemandx";
-static char *cpufreq_gov_conservative = "conservative"; */
+// char *sgov;
+static int gov_val = 2;
+/*char *cpufreq_sysfs_place_holder="/sys/devices/system/cpu/cpu%i/cpufreq/scaling_governor";
+char cpufreq_gov_default[32];
+char *cpufreq_gov_ondemand = "ondemand";
+char *cpufreq_gov_interactive = "interactive";
+char *cpufreq_gov_ondemandx = "ondemandx";
+char *cpufreq_gov_conservative = "conservative"; */
 
 static ssize_t suspend_gov_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
@@ -296,34 +199,6 @@ static ssize_t suspend_gov_store(struct kobject *kobj,
 
 	sscanf(buf, "%d\n", &gov_val);
 	
-/*	if (gov_val == 0) {
-		sgov = &cpufreq_gov_ondemand;
-			pr_info("Suspend Governor: %s\n", cpufreq_gov_ondemand);
-
-	} else if (gov_val == 1) {
-		sgov = &cpufreq_gov_interactive;
-			pr_info("Suspend Governor: %s\n", cpufreq_gov_interactive);
-
-	} else if (gov_val == 2) {
-		sgov = &cpufreq_gov_conservative;
-			pr_info("Suspend Governor: %s\n", cpufreq_gov_conservative);
-
-	} else if (gov_val == 3) {
-		sgov = &cpufreq_gov_ondemandx;
-			pr_info("Suspend Governor: %s\n", cpufreq_gov_ondemandx);
-
-	} else if (gov_val < 0) {
-		gov_val = 0;
-		pr_info("[dtrail] suspend governor error - bailing\n");	
-		return count;
-
-	} else if (gov_val > 3) {
-		gov_val = 3;
-		pr_info("[dtrail] suspend governor error - bailing\n");	
-		return count;
-	}  */
-
-
 	if (gov_val == 0) {
 		strcpy(good_governor, "ondemand");
 			pr_info("Suspend Governor: %s\n", good_governor);
@@ -357,98 +232,6 @@ return count;
 
 }
 
-// char cpufreq_default_gov[CONFIG_NR_CPUS][MAX_GOV_NAME_LEN];
-
-/* void cpufreq_store_default_gov(void)
-{
-unsigned int cpu;
-struct cpufreq_policy *policy;
-
-	for (cpu = 0; cpu < CONFIG_NR_CPUS; cpu++) {
-		policy = cpufreq_cpu_get(cpu);
-		if (policy) {
-			sprintf(cpufreq_default_gov[cpu], "%s",
-					policy->governor->name);
-			cpufreq_cpu_put(policy);
-			}
-		}
-	}
-EXPORT_SYMBOL(cpufreq_store_default_gov); */
-
-
-/*void cpufreq_set_governor(char *good_governor)
-{
-    struct file *scaling_gov = NULL;
-    mm_segment_t old_fs;
-    char    buf[128];
-    int i;
-    loff_t offset = 0;
-
-    if (good_governor == NULL)
-        return;
-
-    for_each_online_cpu(i) {
-        sprintf(buf, cpufreq_sysfs_place_holder, i);
-        scaling_gov = filp_open(buf, O_RDWR, 0);
-        if (scaling_gov != NULL) {
-            if (scaling_gov->f_op != NULL &&
-                scaling_gov->f_op->write != NULL)
-                scaling_gov->f_op->write(scaling_gov,
-                        good_governor,
-                        strlen(good_governor),
-                        &offset);
-            else
-                pr_err("f_op might be null\n");
-
-            filp_close(scaling_gov, NULL);
-        } else {
-            pr_err("%s. Can't open %s\n", __func__, buf);
-        }
-    }
-	set_fs(old_fs);
-}
-EXPORT_SYMBOL(cpufreq_set_governor); */
-
-
-
-/* void cpufreq_save_default_governor(void)
-{
-	struct file *scaling_gov = NULL;
-	mm_segment_t old_fs;
-	char    buf[128];
-	loff_t offset = 0;
-
-
-	old_fs = get_fs();
-	set_fs(KERNEL_DS);
-
-	buf[127] = 0;
-	sprintf(buf, cpufreq_sysfs_place_holder,0);
-	scaling_gov = filp_open(buf, O_RDONLY, 0);
-	if (scaling_gov != NULL) {
-		if (scaling_gov->f_op != NULL &&
-			scaling_gov->f_op->read != NULL)
-			scaling_gov->f_op->read(scaling_gov,
-					cpufreq_gov_default,
-					32,
-					&offset);
-		else
-			pr_err("f_op might be null\n");
-
-		filp_close(scaling_gov, NULL);
-	} else {
-		pr_err("%s. Can't open %s\n", __func__, buf);
-	}
-	set_fs(old_fs);
- }
-EXPORT_SYMBOL(cpufreq_save_default_governor); */
-
-
-
-
-
-
-
 int set_governor(struct cpufreq_policy *policy, char str_governor[16]) {
 	unsigned int ret = -EINVAL;
 	struct cpufreq_governor *t;
@@ -473,54 +256,6 @@ int set_governor(struct cpufreq_policy *policy, char str_governor[16]) {
 }
 EXPORT_SYMBOL(set_governor);
 
-
-
-/*void cpufreq_restore_default_governor(void)
-{
-    cpufreq_set_governor(cpufreq_gov_default);
-}
-EXPORT_SYMBOL(cpufreq_restore_default_governor); */
-
-/* void cpufreq_set_user_governor(void)
-{
-    cpufreq_set_governor(sgov);
-} */
-
-
-/* int cpufreq_change_gov(char *target_gov)
-	{
-	unsigned int cpu = 0;
-	for_each_online_cpu(cpu)
-	return cpufreq_set_gov(target_gov, cpu);
-	pr_info("Suspend Governor: Set");
-	}
-// EXPORT_SYMBOL(cpufreq_change_gov); */
-
-/*int cpufreq_restore_default_gov(void)
-	{
-
-int ret = 0;
-unsigned int cpu;
-
-	for (cpu = 0; cpu < CONFIG_NR_CPUS; cpu++) {
-		if (strlen((const char *)&cpufreq_default_gov[cpu])) {
-			ret = cpufreq_set_gov(cpufreq_default_gov[cpu], cpu);
-	if (ret < 0) */
-
-	/* Unable to restore gov for the cpu as
-	* It was online on suspend and becomes
-	* offline on resume.
-	*/
-/*		pr_info("Unable to restore gov:%s for cpu:%d\n,"
-		, cpufreq_default_gov[cpu]
-		, cpu);
-							}
-		cpufreq_default_gov[cpu][0] = '\0';
-	}
-			return ret;
-}
-EXPORT_SYMBOL(cpufreq_restore_default_gov); */
-
 static ssize_t suspend_gov_version_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
@@ -532,7 +267,7 @@ static ssize_t suspend_gov_version_show(struct kobject *kobj,
 static ssize_t suspend_gov_earlysuspend_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "early suspend active: %u\n", suspend_gov_early_suspend_active);
+	return sprintf(buf, "%u\n", suspend_gov_early_suspend_active);
 }
 
 static struct kobj_attribute suspend_gov_attribute = 
@@ -565,20 +300,17 @@ static void suspend_gov_early_suspend(struct early_suspend *h, struct cpufreq_po
 {
 	mutex_lock(&suspend_mutex);
 
-
-	if (suspend_gov) {
 		suspend_gov_early_suspend_active = true;
 #if 1
 			pr_info("Suspend Governor : Current governor is : %s\n", policy->governor->name);
 		if (policy->governor->name != good_governor) {
 			strcpy(def_governor, policy->governor->name);
 			set_governor(policy, good_governor);
-			change_g = true;
 			pr_info("Suspend Governor : Change governor to : %s\n", policy->governor->name);
+			change_g = true;
 		}
 
 #endif
-	}
 	mutex_unlock(&suspend_mutex);
 }
 
@@ -606,10 +338,6 @@ static int suspend_gov_init(void)
 {
 	int sysfs_result;
 
-	// cpufreq.c
-//	SYMSEARCH_BIND_FUNCTION_TO(cpu_control, __find_governor, __find_governor_s);
-//	SYMSEARCH_BIND_FUNCTION_TO(cpu_control, __cpufreq_set_policy, __cpufreq_set_policy_s);
-
 	suspend_gov_kobj = kobject_create_and_add("suspend_gov", kernel_kobj);
 	if (!suspend_gov_kobj) {
 		pr_err("%s suspend_gov kobject create failed!\n", __FUNCTION__);
@@ -625,7 +353,6 @@ static int suspend_gov_init(void)
 	}
 	return sysfs_result;
 }
-// EXPORT_SYMBOL(suspend_gov_init);
 
 static void suspend_gov_exit(void)
 {
