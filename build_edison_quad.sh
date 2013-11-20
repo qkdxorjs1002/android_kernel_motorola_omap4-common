d1f7f19d40b9c617b5b96b72de0271452cd6b638
dtrail <mnl.manz@gmail.com>
2013-11-20 18:34:45 +0100

    Build Script: Edison: Remove Bootloader CMD line from build script - it's obsolete here. This entries are probably responsible for the bad power consumption on Edison devices.

--- build_edison_quad.sh
+++ build_edison_quad.sh
@@ -27,8 +27,8 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=arm-unknown-linux-gnueabi-
 export TARGET_KERNEL_CUSTOM_TOOLCHAIN=arm-unknown-linux-gnueabi-standard_4.7.2
-export LOCALVERSION="-JBX-1.4-Hybrid-Edison-4.3"
-make -j4 TARGET_BOOTLOADER_BOARD_NAME=edison TARGET_KERNEL_SOURCE=/home/dtrail/android/android_kernel_motorola_omap4-common/ TARGET_KERNEL_CONFIG=mapphone_OCEdison_defconfig BOARD_KERNEL_CMDLINE='root=/dev/ram0 rw mem=1023M@0x80000000 console=null vram=10300K omapfb.vram=0:8256K,1:4K,2:2040K init=/init ip=off mmcparts=mmcblk1:p7(pds),p15(boot),p16(recovery),p17(cdrom),p18(misc),p19(cid),p20(kpanic),p21(system),p22(cache),p23(preinstall),p24(webtop),p25(userdata) mot_sst=1 androidboot.bootloader=0x0A72' $OUT/boot.img
+export LOCALVERSION="-JBX-1.4-Hybrid-4.3"
+make -j4 TARGET_KERNEL_SOURCE=/home/dtrail/android/android_kernel_motorola_omap4-common/ TARGET_KERNEL_CONFIG=mapphone_OCEdison_defconfig $OUT/boot.img
 
 # We don't use the kernel but the modules
 echo "Copying modules to package folder"
