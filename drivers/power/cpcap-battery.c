faecb94e334cea521b35744c9923290edf21bde2
dtrail <mnl.manz@gmail.com>
2014-02-06 21:14:28 +0100

    defconfig: BLX: disable BLX, enable DPLL

--- drivers/power/cpcap-battery.c
+++ drivers/power/cpcap-battery.c
@@ -442,7 +442,8 @@ static void cpcap_batt_ind_chrg_ctrl(struct cpcap_batt_ps *sply)
 		pr_cpcap_batt(TRANSITION, "batt capacity full, chrgcmpl set");
 		sply->ind_chrg_dsbl_time = (unsigned long)temp;
 #ifdef CONFIG_BLX
-	} else if (get_charginglimit() != MAX_CHARGINGLIMIT && sply->batt_state.batt_capacity_one >= get_charginglimit()) {
+	} else if ((get_charginglimit() != MAX_CHARGINGLIMIT && sply->batt_state.batt_capacity_one >= get_charginglimit()) && 
+		   (sply->ac_state.model == CPCAP_BATT_AC_IND)) {
 			pdata->ind_chrg->force_charge_complete(1);
 		pr_cpcap_batt(TRANSITION, "BLX: capacity reached, chrgcmpl set");
 		pr_info("BLX: capacity reached %d, chrgcmpl set\n", get_charginglimit());
