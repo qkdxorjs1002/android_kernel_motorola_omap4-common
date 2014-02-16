fb1260d6d0e0faac834cbdb73a6da60c68398f6b
Alexandr Dvorin <cujo@inbox.ru>
2014-02-16 16:44:04 +0400

    Fix disable first_boot logic in maphhone_panel

--- drivers/video/omap2/displays/panel-mapphone.c
+++ drivers/video/omap2/displays/panel-mapphone.c
@@ -3782,12 +3782,7 @@ static void set_default_panel_init_state(struct omap_dss_device *dssdev)
 }
 static int mapphone_panel_power_on(struct omap_dss_device *dssdev)
 {
-#ifdef CONFIG_PANEL_MAPPHONE_SKIP_FIRSTBOOT
-	static bool first_boot = false;
-#else
 	static bool first_boot = true;
-#endif
-
 	struct mapphone_data *mp_data = dev_get_drvdata(&dssdev->dev);
 	int ret;
 	u8 power_mode = 0;
@@ -3814,6 +3809,20 @@ static int mapphone_panel_power_on(struct omap_dss_device *dssdev)
 	if (ret) {
 		dev_err(&dssdev->dev, "failed to enable DSI\n");
 		goto err0;
+	}
+
+#ifdef CONFIG_PANEL_MAPPHONE_SKIP_FIRSTBOOT
+	static bool skip_first_boot = true;
+#else
+	static bool skip_first_boot = false;
+#endif
+
+	if ((skip_first_boot || !first_boot) && !dssdev->phy.dsi.d2l_use_ulps) {
+		if (dssdev->platform_enable) {
+			ret = dssdev->platform_enable(dssdev);
+			if (ret)
+				goto err0;
+		}
 	}
 
 #ifdef CONFIG_FB_OMAP_BOOTLOADER_INIT
@@ -3845,14 +3854,6 @@ static int mapphone_panel_power_on(struct omap_dss_device *dssdev)
 			clk_disable(clk);
 	}
 #endif
-
-	if (!first_boot && !dssdev->phy.dsi.d2l_use_ulps) {
-		if (dssdev->platform_enable) {
-			ret = dssdev->platform_enable(dssdev);
-			if (ret)
-				goto err0;
-		}
-	}
 
 	mapphone_hw_reset(dssdev);
 
