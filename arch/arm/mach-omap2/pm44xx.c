6c4691975e686eb5fb3f66aa0df8477067fdff02
dtrail <mnl.manz@gmail.com>
2014-02-22 16:00:28 +0100

    omap: pm: use workaround for CK PAD differential only on edison/targa

--- arch/arm/mach-omap2/pm44xx.c
+++ arch/arm/mach-omap2/pm44xx.c
@@ -1137,7 +1137,7 @@ static void __init syscontrol_setup_regs(void)
 		OMAP4_CTRL_MODULE_PAD_CORE_CONTROL_LPDDR2IO2_3);
 
 	syscontrol_lpddr_clk_io_errata(true);
-	
+#if defined(CONFIG_MAPPHONE_TARGA) || defined(CONFIG_MAPPHONE_EDISON)	
 	 /*
 	  * dtrail:  Workaround for CK differential IO PADn, PADp values due to bug in
 	  *       EMIF CMD phy.
@@ -1148,6 +1148,7 @@ static void __init syscontrol_setup_regs(void)
 	 v = omap4_ctrl_pad_readl(OMAP4_CTRL_MODULE_PAD_CORE_CONTROL_LPDDR2IO2_2);
 	 v &= ~OMAP4_LPDDR2IO2_GR10_WD_MASK;
 	 omap4_ctrl_pad_writel(v, OMAP4_CTRL_MODULE_PAD_CORE_CONTROL_LPDDR2IO2_2);
+#endif
 }
 
 static void __init prcm_setup_regs(void)
