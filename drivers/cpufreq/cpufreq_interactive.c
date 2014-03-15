a41bf4af87038e2171d12482e4074b2085ea0f28
dtrail <mnl.manz@gmail.com>
2014-03-15 13:04:38 +0100

    omap: cpufreq_interactive: fix

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -118,14 +118,6 @@ static unsigned long min_sample_time;
  */
 #define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
 static unsigned long timer_rate;
-<<<<<<< HEAD
-
-=======
-#ifdef CONFIG_OMAP4_DPLL_CASCADING
-if (likely(dpll_active))
-static unsigned long default_timer_rate;
-#endif
->>>>>>> parent of f5386a7... omap: dpll_cascading: module: fix
 
 /*
  * Wait this long before raising speed above hispeed, by default a single
@@ -166,24 +158,6 @@ struct cpufreq_governor cpufreq_gov_interactive = {
 	.owner = THIS_MODULE,
 };
 
-<<<<<<< HEAD
-=======
-#ifdef CONFIG_OMAP4_DPLL_CASCADING
-	if (likely(dpll_active)) {
-void cpufreq_interactive_set_timer_rate(unsigned long val, unsigned int reset)
-{
-	if (!reset) {
-		default_timer_rate = timer_rate;
-		timer_rate = val;
-	} else {
-		if (timer_rate == val)
-			timer_rate = default_timer_rate;
-	}
-}
-}
-#endif
-
->>>>>>> parent of f5386a7... omap: dpll_cascading: module: fix
 static void cpufreq_interactive_timer(unsigned long data)
 {
 	unsigned int delta_idle;
@@ -1265,10 +1239,6 @@ static int __init cpufreq_interactive_init(void)
 	min_sample_time = DEFAULT_MIN_SAMPLE_TIME;
 	above_hispeed_delay_val = DEFAULT_ABOVE_HISPEED_DELAY;
 	timer_rate = DEFAULT_TIMER_RATE;
-#ifdef CONFIG_OMAP4_DPLL_CASCADING
-	if (likely(dpll_active))
-	default_timer_rate = DEFAULT_TIMER_RATE;
-#endif
 
 	sampling_periods = DEFAULT_SAMPLING_PERIODS;
 	hi_perf_threshold = DEFAULT_HI_PERF_THRESHOLD;
